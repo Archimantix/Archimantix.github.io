331c3996e458ea57bc8a39b7010d0e9009201860
Archimantix <s.ganjisaffar@gmail.com>
2024-08-21 00:58:26 -0700

    avatar added.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724225453',resources: [
+    cacheName: 'chirpy-1724226909',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
